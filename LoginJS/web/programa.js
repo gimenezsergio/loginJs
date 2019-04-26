e445940472ae7f4af5036a9a48eb1a6c37fd4aa0
sergio <sergio@secondary.home>
2019-04-26 17:58:24 -0300

    Array de usuarios.

--- LoginJS/web/programa.js
+++ LoginJS/web/programa.js
@@ -1,5 +1,29 @@
 class Usuario {
     
+    static registroClave(){        
+        let claveRegistrada = "123";
+        return claveRegistrada;
+    }
+    
+    static registroNombre(){
+        let usuarioRegistrado = "poaneleNati";
+        return usuarioRegistrado;
+    }
+    
+    static registroMuchosUsuarios(){
+        let listado = [];
+        listado.push("Pepe");
+        listado.push("Gio");
+        listado.push("Marcos");
+        listado.push("Nati");
+        listado.push("Sergio");
+        console.log(listado);
+        
+        for ( let i = 0; i < 6; i++) {
+            console.log("Nombre: " + listado[i]);
+        }
+    }
+    
     static entrar(){
         console.log("Estas en el metodo entrar, cahbon.");
         console.log(document.querySelector("#usuario").value);
@@ -7,10 +31,22 @@ class Usuario {
         let miClave = document.querySelector("#clave").value;
         document.querySelector("#resultadoPnl").innerHTML = miClave 
                 + " " + miUsuario;
+        
+        //CONSEGUIMOS USUARIO Y CLAVES REGISTRADAS
+        console.log(Usuario.registroNombre());
+        console.log(Usuario.registroClave());
+        
+        //COMPARAR USAURIO Y CLAVE DE FORMULARIO Y REGISTRADAS
+        if (miUsuario == Usuario.registroNombre() && miClave == Usuario.registroClave()) {
+            console.log("Si, adentro!!!");
+        } else {
+            console.log("No, nada que ver!  a la calle!");
+        }
     }
     
     static init(){
         document.querySelector("#entrarBtn").setAttribute("onclick","Usuario.entrar();");
+        Usuario.registroMuchosUsuarios();
     }
     
 }
